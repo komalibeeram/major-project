e5cdef4f05381eacc22e6ad753d5d7d707317783
gdpr1504 <gondesia.nani@gmail.com>
2022-05-25 00:01:28 +0530

    backend flow

--- Backend/image_preprocessing/edge_detection.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-from image_helper_functions import *
-
-def edge_detection(image):
-    kernel = np.ones((5,5),'uint8')
-    image = cv2.dilate(image,kernel,iterations=1)
-    edges = cv2.Canny(image, 100, 70)
-    return edges
--- Backend/image_preprocessing/main.py
+++ Backend/image_preprocessing/main.py
@@ -1,52 +1,77 @@
 from image_helper_functions import *
-from grayscale import grayscale
-from edge_detection import edge_detection
-from contour_detection import contour_detection, locate_document
+from preprocess_methods import *
 import cv2
+import pytesseract
+from PIL import Image
 
-
-
-path = "/images/username_image" # image path
+path = "C:/Users/hp/Documents/major-project/Backend/images/" # image path
 
 def preprocessing_pipeline(image,path):
-
+    # is image shadowed
+    image=shadow(image)
+    save_image(image,'_first.png',path,)
+    # noise removal
+    image=denoise(image)
+    save_image(image,'_first2.png',path,)
+    contours = contour_detection(image)
+    save_image(image,'_first3.png',path,)
+    # save_image(contours,'_detected_document',path,)
     # is image grayscale
-    if grayscale(image,path):
-        image = load_image(path+'_grayscaled.png')
+    # image = grayscale(image,path)
     
+    # binarization
+    # image = morphology(image)
+
     # edge detection
-    edges = edge_detection(image)
+    # edges = edge_detection(image)
 
     # document detection
     # contour detection
-    contours = contour_detection(edges)
-    image = locate_document(image, contours)
-    save_image(image,'_detected_document',path,)
+
 
     # skewness correction
-
-    # image scaling correction
-    
+    # deskew = skew_correction(image)
+    # image = rotate(image, skew_correction(image), cval=1)
+    # save_image(deskew,'_deskewed',path,)
+    # save_image(image,'_first4.png',path,)
 
     # blur correction
+    image = blur_correction(image)
+    # save_image(sharpen, '_deblurred',path,)
+    # save_image(image,'_first5.png',path,)
 
-    # is image shadowed
-
-    # binarization
-
-    # noise removal
+    
+    # save_image(shadowed_img, '_shadow',path,)
+    contours = contour_detection(image)
+    save_image(image,'_last.png',path,)
+    
+    save_image(image,'_final1.png',path,)
+    # display_image(image,"final")
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+def get_string(img_path):
+    # Recognize text with tesseract for python
+    result = pytesseract.image_to_string(Image.open(img_path))
 
+    # Remove template file
+    #os.remove(temp)
+
+    return result
 def main():
     global path
 
     # load_image
-    img = load_image(path+'_original.jpg')
-    display_image(img,"Original")
+    image = load_image('C:/Users/hp/Documents/major-project/Backend/images/Capture.PNG') #TODO: change img path
+    # display_image(image,"Original")
 
     preprocessing_pipeline(image,path)
+    print ('--- Start recognize text from image ---')
+    print (get_string('C:/Users/hp/Documents/major-project/Backend/images/_final1.png'))
+
+    print ("------ Done -------")
+
+
 
 if __name__ == '__main__':
     main()
